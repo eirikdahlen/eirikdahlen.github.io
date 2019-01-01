7a930dbbabf05425dab7a90aa7608388a09c2beb
Eirik Dahlen <dahleneirik@gmail.com>
2019-01-01 17:27:10 +0100

    #2 created jumbotron

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Jumbotron } from 'reactstrap';
+
+const Header = () => {
+    return(
+        <div>
+            <Jumbotron className="jumbotron">
+                <h1>Header</h1>
+            </Jumbotron>
+        </div>
+
+    )
+
+}
+
+
+export default Header;
