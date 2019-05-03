a0d31aaf4e531c88d907d8fac7566b394af7aca6
Eirik Dahlen <dahleneirik@gmail.com>
2019-05-03 13:48:34 +0200

    Fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div>
             <p className="footer_p">
                 DISCLAIMER: These compendiums are written by me, and originally for me. Some will be in English
-                and some will be in Norwegian. I do not claim that the content is 100% or accurate, but it is what
+                and some will be in Norwegian. I do not claim that the content is 100% accurate, but it is what
                 I learned during the courses and my opinion of what is correct. The curriculum may have changed
                 since I did these courses myself. You are allowed to download the pdf-files, but not to edit
                 the files in any way. Thank you!
